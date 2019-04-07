85b1bdf100499bf49877542a79d51b7154756cc6
pkolomeitsev <p.kolomeitsev@gmail.com>
2019-04-08 01:34:50 +0300

    update def controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\PropelModels\Map\UsersTableMap;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +14,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $table = UsersTableMap::getTableMap();
+
+        echo sprintf('Analyze table `%s`:' . PHP_EOL, $table->getName());
+        foreach ($table->getColumns() as $column) {
+            echo '- ' . $column->getName() . PHP_EOL;
+        }
+
+        die;
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+//        return $this->render('default/index.html.twig', [
+//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+//        ]);
     }
 }
